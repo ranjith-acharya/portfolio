0fe87cdac49d3ba4434d32468ce98fbbadf2e2cc
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2024-08-19 01:21:46 +0530

    StairEffects Transition Component

---
+++ components/Stairs.jsx
@@ -0,0 +1,42 @@
+import { animate, motion } from "framer-motion";
+
+const stairAnimation = {
+    initial: {
+        bottom: "0%"
+    },
+    animate: {
+        bottom: "100%"
+    },
+    exit: {
+        bottom: ["100%", "0%"],
+    },
+}
+
+const reverseIndex = (index) => {
+    const totalSteps = 7;
+    return totalSteps - index - 1;
+}
+
+const Stairs = () => {
+  return (
+    <>
+        {[...Array(7)].map((_, index) => {
+            return ( <motion.div 
+                key={index} 
+                variants={stairAnimation} 
+                initial="initial" 
+                animate="animate"
+                exit="exit"
+                transition={{
+                    duration: 0.4,
+                    ease: "easeInOut",
+                    delay: reverseIndex(index) * 0.1
+                }}
+                className="h-full w-full bg-white border-none relative"
+            />)
+        })}
+    </>
+  );
+}
+
+export default Stairs
