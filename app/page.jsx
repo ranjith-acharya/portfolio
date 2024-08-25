0da073518c9185725a4a845a13913a4214637ea9
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2024-08-25 21:54:40 +0530

    Update the Homepage component

--- app/page.jsx
+++ app/page.jsx
@@ -17,10 +17,12 @@ const Home = () => {
           Turning complex ideas into scalable, innovative solutions with efficient software engineering.
           </p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-              <span>Download CV</span>
-              <FiDownload className="text-xl"/>
-            </Button>
+            <a href="/assets/resume/RanjithKrishna_Acharya - Resume.pdf" download>
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+            </a>
             <div className="mb-8 xl:mb-0 ">
               <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
             </div>
